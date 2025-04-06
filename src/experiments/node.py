c5f0f2304b03c9285c72a56076c2c235133b4d02
Jeffrey <jeffliang02@gmail.com>
2025-04-06 00:26:51 +0000

    Ported functions to .py files

---
+++ src/experiments/node.py
@@ -0,0 +1,127 @@
+import torch
+from pytorch3d import Meshes
+from ddn import EqConstDeclarativeNode, DeclarativeFunction
+from functions import *
+from utils import *
+import scipy.optimize as opt
+
+class ConstrainedProjectionNode(EqConstDeclarativeNode):
+    """
+    Performs a projection of the input points X onto the nearest points Y such that the volume of Y is constant.
+    """
+    def __init__(self, src: Meshes, tgt: Meshes):
+        super().__init__(eps=1.0e-6) # relax tolerance on optimality test 
+        self.src = src # source meshes (B,)
+        # self.tgt = tgt # target meshes (B,)
+        self.b = len(src)
+
+    def objective(self, xs: torch.Tensor, y: torch.Tensor, scatter_add=False):
+        """
+        Calculates sum of squared differences between source and target meshes.
+
+        Args:
+            xs (torch.Tensor): a padded (B, max Vi, 3) tensor of the original vertices
+            y (torch.Tensor): a padded (B, max Vi, 3) tensor of the projected vertices        
+        """
+        src_verts = padded_to_packed(xs, self.src.num_verts_per_mesh()).view(-1,3)
+        tgt_verts = padded_to_packed(y, self.src.num_verts_per_mesh()).view(-1,3)
+
+        sqr_diffs = torch.square(src_verts - tgt_verts).sum(dim=-1) # (sum(V_i))
+        n_batches = len(self.src)
+        sse = torch.zeros(n_batches, dtype=sqr_diffs.dtype)
+        if scatter_add:
+            sse.scatter_add_(0, self.src.verts_packed_to_mesh_idx(), sqr_diffs)
+        else:
+            n_verts_per_mesh = self.src.num_verts_per_mesh()
+            for i in range(n_batches):
+                mesh_to_vert = self.src.mesh_to_verts_packed_first_idx()  # Index of first face per mesh
+                start = mesh_to_vert[i]
+                end = start + n_verts_per_mesh[i]
+                sse[i] = sqr_diffs[start:end].sum()  # Sum over all faces
+        return sse
+
+    def equality_constraints(self, xs, y, scatter_add=False):
+        """
+        Enforces volume constraint of projected points
+        Assumes same number of vertices in each projected mesh currently
+
+        Args:
+            xs (torch.Tensor): a padded (B, max Vi, 3) tensor of the original vertices
+            y (torch.Tensor): a padded (B, max Vi, 3) tensor of the projected vertices
+        """
+        n_batches = len(self.src)
+        y_packed = padded_to_packed(y, self.src.num_verts_per_mesh())
+        verts_packed = y_packed.view(-1,3) # (sum(V_i), 3)
+
+        faces_packed = self.src.faces_packed()  # (sum(F_i), 3)
+        face_vertices = verts_packed[faces_packed]  # (sum(F_i), 3, 3)
+        
+        # Calculate tetrahedron volumes for each face
+        v0, v1, v2 = face_vertices[:, 0, :], face_vertices[:, 1, :], face_vertices[:, 2, :]
+        cross_product = torch.cross(v0, v1, dim=-1)  # (F, 3)
+        face_volumes = torch.sum(cross_product * v2, dim=-1) / 6.0  # (F,)
+        volumes = torch.zeros(n_batches, device=verts_packed.device, dtype=face_volumes.dtype)
+        if scatter_add:
+            volumes.scatter_add_(0, self.src.faces_packed_to_mesh_idx(), face_volumes)
+        else:
+            n_faces_per_mesh = self.src.num_faces_per_mesh()
+            for i in range(n_batches):
+                mesh_to_face = self.src.mesh_to_faces_packed_first_idx()  # Index of first face per mesh
+                start = mesh_to_face[i]
+                end = start + n_faces_per_mesh[i]
+                volumes[i] = face_volumes[start:end].sum()  # Sum over all faces
+
+        volumes = volumes.abs()
+        return volumes  # Shape: (B,)    
+    
+    def solve(self, xs: torch.Tensor):
+        """Projects the vertices onto the target mesh vertices across batches.
+
+        Args:
+            xs (torch.Tensor): a padded (B, max Vi, 3) tensor of vertices in the batched meshes
+
+        Returns:
+            results (torch.Tensor): a padded (B, max Vi, 3) tensor of the projected vertices
+        """
+        n_batches = len(self.src)
+        num_verts_per_mesh = self.src.num_verts_per_mesh()
+        results = torch.zeros((n_batches, num_verts_per_mesh.max(), 3), dtype=torch.double)
+        for batch in range(n_batches):
+            n_verts = num_verts_per_mesh[batch]
+            verts = xs[batch][:n_verts].flatten().detach().double().cpu().numpy()
+            faces = self.src[batch].faces_packed().detach().int().cpu().numpy()
+            
+            # Y = xs[batch][:n_verts].flatten().detach().double().cpu() * 2
+            # Y = Y.numpy()
+            Y = xs[batch][:n_verts].flatten().detach().double().cpu().numpy() 
+
+            with torch.no_grad():
+                src_vtx = self.src[batch].verts_packed().detach()
+                src_faces = self.src[batch].faces_packed().detach()
+                vol = calculate_volume(src_vtx, src_faces)
+
+            eq_constraint = {
+                'type': 'eq',
+                'fun' : lambda u: volume_constraint(u, faces, vol).cpu().numpy(),
+                'jac' : lambda u: volume_constraint_grad(u, faces).cpu().numpy(),
+            }
+
+            res = opt.minimize(
+                lambda u: least_squares(u, verts).detach().cpu().numpy(),
+                Y,
+                method='SLSQP',
+                jac=lambda u: least_squares_grad(u, verts).cpu().numpy(),
+                constraints=[eq_constraint],
+                options={'ftol': 1e-4, 'iprint': 2, 'maxiter': 100}
+            )
+
+            if not res.success:
+                print("FAILED:", res.message)
+            results[batch] = torch.tensor(res.x, dtype=torch.double, requires_grad=True).view(-1,3)
+        results = torch.nn.utils.rnn.pad_sequence(results, batch_first=True)
+        return results,None
+    
+
+class ConstrainedProjectionFunction(DeclarativeFunction):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
