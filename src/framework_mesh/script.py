c5069c90372e53484f7738ea875a659bdc28b8e1
jeffreyliang <u7013004@anu.edu.au>
2025-04-19 10:30:17 +1000

    Changed to Plotly visualisation

--- src/framework_mesh/script.py
+++ src/framework_mesh/script.py
@@ -14,20 +14,20 @@ import warnings
 warnings.filterwarnings('ignore')
 
 def main():
-    data_path = "/home/jeffrey/honours/src/experiments/skyconfig_local.json"
+    data_path = "/Users/jeffreyliang/Documents/Honours/honours/src/framework_mesh/skyconfig_local.json"
     with open(data_path, 'r') as f:
         config = json.load(f)
 
     # Modify the 'mesh_res' value
     config['paths']['mesh_res'] = 2
     dataloader = DataLoader(config)
-    runner = ExperimentRunner("/home/jeffrey/honours/src/experiments/exp_pipeline.json", dataloader)
+    runner = ExperimentRunner("/Users/jeffreyliang/Documents/Honours/honours/src/framework_mesh/exp_pipeline.json", dataloader)
     runner.run()
 
-    config['paths']['mesh_res'] = 3
-    dataloader = DataLoader(config)
-    runner = ExperimentRunner("/home/jeffrey/honours/src/experiments/exp_pipeline.json", dataloader)
-    runner.run()
+    # config['paths']['mesh_res'] = 3
+    # dataloader = DataLoader(config)
+    # runner = ExperimentRunner("/home/jeffrey/honours/src/experiments/exp_pipeline.json", dataloader)
+    # runner.run()
 
 
 if __name__ == "__main__":
