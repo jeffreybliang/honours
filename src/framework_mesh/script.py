1ede17374bf6443f9a916afdc3888de057038709
jeffreyliang <u7013004@anu.edu.au>
2025-05-05 22:08:14 +1000

    ready to run on cupertino

--- src/framework_mesh/script.py
+++ src/framework_mesh/script.py
@@ -1,11 +1,9 @@
 import torch
 import json
-from itertools import product
 from .dataloader import DataLoader
 from .experimentrunner import ExperimentRunner
 import warnings
 warnings.filterwarnings("ignore")
-
 
 def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -19,28 +17,29 @@ def main():
 
     with open(exp_config_path, 'r') as f:
         exp_config = json.load(f)
-    
-    methods = ["jacobi"]
-    constrained_options = [False]
-    mesh_resolutions = [2]
+
+    smoothing = exp_config.get("gradient", {}).get("smoothing", False)
+    mesh_resolutions = [2, 3]
 
     for mesh_res in mesh_resolutions:
-        # Update mesh resolution
         data_config['paths']['mesh_res'] = mesh_res
         dataloader = DataLoader(data_config, device)
 
-        for method in methods:
-            for constrained in constrained_options:
-                # Update gradient method and constraint setting
-                exp_config['gradient']['method'] = method
-                exp_config['gradient']['constrained'] = constrained
+        if not smoothing:
+            exp_config['name'] = f"pipeline_nosmooth_res_{mesh_res}"
+            print(f"[RUN] smoothing=False | mesh_res={mesh_res}")
+            runner = ExperimentRunner(exp_config, dataloader)
+            runner.run()
 
-                # Update experiment name to be descriptive
-                exp_config['name'] = f"pipeline_{method}_constrained_{constrained}_res_{mesh_res}"
-
-                # Create runner and run
-                runner = ExperimentRunner(exp_config, dataloader)
-                runner.run()
+        else:
+            method = "jacobi"
+            constrained = False
+            exp_config['gradient']['method'] = method
+            exp_config['gradient']['constrained'] = constrained
+            exp_config['name'] = f"pipeline_{method}_constrained_{constrained}_res_{mesh_res}"
+            print(f"[RUN] smoothing=True | method={method} | constrained={constrained} | mesh_res={mesh_res}")
+            runner = ExperimentRunner(exp_config, dataloader)
+            runner.run()
 
 if __name__ == "__main__":
     main()
