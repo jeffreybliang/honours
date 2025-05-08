19da6b4288df72ac4d37eff2b4bbe1fe650c1543
jeffreyliang <u7013004@anu.edu.au>
2025-05-08 15:43:22 +1000

    just doing shit

--- src/framework_mesh/script.py
+++ src/framework_mesh/script.py
@@ -7,12 +7,32 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
+import torch
+import json
+from framework_mesh.dataloader import DataLoader
+from framework_mesh.experimentrunner import ExperimentRunner
+
 def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     device = torch.device("cpu")
     
-    data_path = "/Users/jeffreyliang/Documents/Honours/honours/src/framework_mesh/skyconfig_balloon.json"
-    exp_config_path = "/Users/jeffreyliang/Documents/Honours/honours/src/framework_mesh/exp_gradients.json"
+    # --- First experiment ---
+    # data_path = "/Users/jeffreyliang/Documents/Honours/honours/src/framework_mesh/skyconfig_balloon.json"
+    # exp_config_path = "/Users/jeffreyliang/Documents/Honours/honours/src/framework_mesh/exp_gradients.json"
+
+    # with open(data_path, 'r') as f:
+    #     data_config = json.load(f)
+
+    # with open(exp_config_path, 'r') as f:
+    #     exp_config = json.load(f)
+
+    # dataloader = DataLoader(data_config, device)
+    # runner = ExperimentRunner(exp_config, dataloader)
+    # runner.run()
+
+    # --- Second experiment ---
+    data_path = "/Users/jeffreyliang/Documents/Honours/honours/src/framework_mesh/skyconfig_strawberry.json"
+    exp_config_path = "/Users/jeffreyliang/Documents/Honours/honours/src/framework_mesh/exp_strawberry.json"
 
     with open(data_path, 'r') as f:
         data_config = json.load(f)
@@ -23,6 +43,9 @@ def main():
     dataloader = DataLoader(data_config, device)
     runner = ExperimentRunner(exp_config, dataloader)
     runner.run()
+
+if __name__ == "__main__":
+    main()
 
 if __name__ == "__main__":
     main()
