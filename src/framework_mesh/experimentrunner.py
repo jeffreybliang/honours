ba696cde02455d65e19b28bc2c3dff607e5fd131
jeffreyliang <u7013004@anu.edu.au>
2025-05-09 05:15:05 +1000

    added penalty files going to run script now

--- src/framework_mesh/experimentrunner.py
+++ src/framework_mesh/experimentrunner.py
@@ -10,7 +10,7 @@ import random
 import wandb
 import os
 from tqdm import trange
-
+from .penalty import PenaltyMethod
 
 class ExperimentRunner:
     def __init__(self, experiment_config: Union[str, dict], data_loader: DataLoader) -> None:
@@ -66,6 +66,10 @@ class ExperimentRunner:
         self.velocity_enabled = velocity_cfg.get("enabled", False)
         self.velocity_k = velocity_cfg.get("k", 0)
         self.beta = velocity_cfg.get("beta", 1)
+
+        self.lambda_vol = self.cfg["penalty"]["lambda_vol"]
+        self.method = self.cfg.get("method", "projection")
+
 
 # ============================================================================================================
 
@@ -133,19 +137,29 @@ class ExperimentRunner:
                         "velocity/mean_disp_z": mean_disp[2].item(),
                     }, step=step_offset)
 
-            # Train the mesh transformation
-            final_verts = self.train_loop(
-                src_mesh, tgt_mesh,
-                projmats, edgemap_info,
-                gt_projmats, gt_edgemap_info,
-                n_iters=self.n_iters,
-                step_offset=step_offset,
-                lr=self.lr,
-                moment=self.momentum,
-                device=device,
-                target_volume=initial_volume
-            )
-            
+            if self.method == "projection":
+                final_verts = self.train_loop(                
+                    src_mesh, tgt_mesh,
+                    projmats, edgemap_info,
+                    gt_projmats, gt_edgemap_info,
+                    n_iters=self.n_iters,
+                    step_offset=step_offset,
+                    lr=self.lr,
+                    moment=self.momentum,
+                    device=device,
+                    target_volume=initial_volume
+                    )
+            else:
+                final_verts = self.train_loop_penalty(src_mesh, tgt_mesh,
+                                                    projmats, edgemap_info,
+                                                    gt_projmats, gt_edgemap_info,
+                                                    n_iters=self.n_iters,
+                                                    step_offset=step_offset,
+                                                    lr=self.lr,
+                                                    moment=self.momentum,
+                                                    device=device)
+
+
             if prev_verts is not None:
                 prev_displacement = final_verts[0] - prev_verts[0]
             prev_verts = final_verts.detach()
@@ -160,23 +174,26 @@ class ExperimentRunner:
 
 # ============================================================================================================
 
-    def train_loop(self, src: Meshes, tgt: Meshes, projmats, edgemap_info, gt_projmats, gt_edgemap_info, 
-                n_iters, step_offset, lr, moment, device, target_volume):
-        node = ConstrainedProjectionNode(src, target_volume, self.wandb)
+    def train_loop(self, src, tgt, projmats, edgemap_info, gt_projmats, gt_edgemap_info,
+                n_iters, step_offset, lr, moment, device: torch.device, target_volume=None):
         verts_init = src.verts_padded()
         verts_init.requires_grad = True
-        verts = verts_init.clone().detach().requires_grad_(True).to(device)
-        V_max = src.num_verts_per_mesh().max().item()
-        boundary_mask = torch.zeros(V_max, dtype=torch.bool, device=device)
+
+        if self.method == "penalty":
+            xs = verts_init.clone().detach().requires_grad_(True).to(device)
+        else:
+            verts = verts_init.clone().detach().requires_grad_(True).to(device)
 
         faces = src[0].faces_packed().to(device)
-        edge_src, edge_dst = build_edge_lists(faces, device)
-        all_idx = torch.arange(V_max, device=device)
-        D_all = bfs_hop_distance(V_max, edge_src, edge_dst, all_idx, k_max=10)
+        V = verts_init.size(1)
+        boundary_mask = torch.zeros(V, dtype=torch.bool, device=device)
 
         if self.smoothing:
+            edge_src, edge_dst = build_edge_lists(faces, device)
+            all_idx = torch.arange(V, device=device)
+            D_all = bfs_hop_distance(V, edge_src, edge_dst, all_idx, k_max=10)
             hook = select_hook(
-                method=self.smoothing_method,         # "jacobi", "invhop", or "khop"
+                method=self.smoothing_method,
                 edge_src=edge_src,
                 edge_dst=edge_dst,
                 boundary_mask=boundary_mask,
@@ -185,75 +202,77 @@ class ExperimentRunner:
                 constrained=self.smoothing_constrained,
                 debug=self.smoothing_debug,
             )
-            verts.register_hook(hook)
+            if self.method == "penalty":
+                xs.register_hook(hook)
+            else:
+                verts.register_hook(hook)
 
-        chamfer_loss = PyTorchChamferLoss(src, tgt, projmats, edgemap_info, boundary_mask=boundary_mask, doublesided = self.cfg["chamfer"]["doublesided"])
-        optimiser = torch.optim.SGD([verts], lr=lr, momentum=moment)
-        a,b = edgemap_info
-        a,b = a[0], b[0]
-        projectionplot = plot_projections(verts.detach().squeeze().double(), gt_projmats, gt_edgemap_info)
-        cmin,cmax = None,None
-        bmin,bmax = None,None
+        if self.method == "penalty":
+            loss_fn = PenaltyMethod(src, tgt, projmats, edgemap_info,
+                                    lambda_vol=self.lambda_vol, boundary_mask=boundary_mask)
+            optimiser = torch.optim.SGD([xs], lr=lr, momentum=moment)
+        else:
+            node = ConstrainedProjectionNode(src, target_volume, self.wandb)
+            loss_fn = PyTorchChamferLoss(src, tgt, projmats, edgemap_info, boundary_mask=boundary_mask)
+            optimiser = torch.optim.SGD([verts], lr=lr, momentum=moment)
 
-        initheatmap,cmin,cmax = create_heatmap(Meshes(verts=verts.detach(), faces=src[0].faces_packed().unsqueeze(0)), tgt[0], cmin, cmax)
+        a, b = edgemap_info
+        a, b = a[0], b[0]
+        mesh_input = xs if self.method == "penalty" else verts
+        projectionplot = plot_projections(mesh_input.detach().squeeze().double(), gt_projmats, gt_edgemap_info)
+        cmin, cmax = None, None
+        initheatmap, cmin, cmax = create_heatmap(Meshes(verts=mesh_input.detach(), faces=faces.unsqueeze(0)), tgt[0], cmin, cmax)
+
         if self.wandb:
             wandb.log({"plt/projections": wandb.Image(projectionplot),
-                        "plt/heatmap": wandb.Plotly(initheatmap)}, step= step_offset)
+                    "plt/heatmap": wandb.Plotly(initheatmap)}, step=step_offset)
 
         min_loss = float("inf")
         best_verts = None
-    
-        pbar = trange(n_iters, desc="Training", leave=True, mininterval=5)  # Always visible
+        pbar = trange(n_iters, desc="Training", leave=True)
+
         for i in pbar:
             optimiser.zero_grad(set_to_none=True)
-            node.iter = i + step_offset
-            projverts = ConstrainedProjectionFunction.apply(node, verts)
-            loss = chamfer_loss(projverts)
 
-            colour = bcolors.FAIL
+            if self.method == "penalty":
+                loss_fn.iter = i + step_offset
+                loss = loss_fn(xs)
+                projverts = xs
+            else:
+                node.iter = i + step_offset
+                projverts = ConstrainedProjectionFunction.apply(node, verts)
+                loss = loss_fn(projverts)
+
             if loss.item() < min_loss:
                 min_loss = loss.item()
                 best_verts = projverts.detach().clone()
-                colour = bcolors.OKGREEN
-                
+
             loss.backward()
             optimiser.step()
+            pbar.set_description(f"Loss: {loss.item()*10:.4f}")
 
-            pbar.set_description(f"Loss: {loss.item():.4f}")
-
-
-            tmp_mesh = Meshes(verts=projverts.detach(), faces=src[0].faces_packed().unsqueeze(0))
-            # log
             if self.wandb:
-                wandb.log(
-                    data = {
-                    "outer/chamfer": loss * 10,
+                wandb.log({
+                    "outer/chamfer": loss * (10 if self.method == "projection" else 1),
                     "outer/vol": calculate_volume(projverts[0], src[0].faces_packed()),
-                    "outer/gt/chamfer": chamfer_gt(tmp_mesh, tgt)[0],
+                    "outer/gt/chamfer": chamfer_gt(projverts, src, tgt)[0] * (10 if self.method == "projection" else 1),
                     "outer/gt/iou": iou_gt(projverts, src, tgt)[0]
-                    },
-                    step = i + step_offset
-                )
+                }, step=i + step_offset)
 
             if self.verbose:
-                print(f"{i:4d} Loss: {colour}{loss.item():.3f}{bcolors.ENDC} Volume: {calculate_volume(projverts[0], src[0].faces_packed()):.3f}")
-                print(f"GT Chamfer: [{', '.join(f'{x:.3f}' for x in chamfer_gt(tmp_mesh, tgt))}] "
+                print(f"{i:4d} Loss: {loss.item():.3f} Volume: {calculate_volume(projverts[0], src[0].faces_packed()):.3f}")
+                print(f"GT Chamfer: [{', '.join(f'{x:.3f}' for x in chamfer_gt(projverts, src, tgt))}] "
                     f"GT IoU: [{', '.join(f'{x:.3f}' for x in iou_gt(projverts, src, tgt))}]")
-            def should_log(i):
-                return i < 50 or (i % self.vis_freq == self.vis_freq-1)  # True at i = 1, 2, 4, 8, 16, ...
-            if self.vis_enabled and should_log(i):
+
+            if self.vis_enabled and i % self.vis_freq == self.vis_freq - 1:
+                heatmap, cmin, cmax = create_heatmap(Meshes(verts=projverts.detach(), faces=faces.unsqueeze(0)), tgt[0], cmin, cmax)
                 projectionplot = plot_projections(projverts.detach().squeeze().double(), gt_projmats, gt_edgemap_info)
-                heatmap,cmin,cmax = create_heatmap(tmp_mesh, tgt[0], cmin, cmax)
-                boundary_fig, bmin, bmax = compute_boundary_distance_heatmap(
-                    tmp_mesh, boundary_mask, D_all, bmin, bmax
-                )
                 if self.wandb:
                     wandb.log({
-                         "plt/projections": wandb.Image(projectionplot),
-                         "plt/heatmap": wandb.Plotly(heatmap),
-                         "plt/boundary_heatmap": wandb.Plotly(boundary_fig)
-                         }, 
-                         step=i + step_offset)        
+                        "plt/projections": wandb.Image(projectionplot),
+                        "plt/heatmap": wandb.Plotly(heatmap)
+                    }, step=i + step_offset)
+
         return best_verts
 
     def get_gt_mesh(self,name):
@@ -269,50 +288,32 @@ class ExperimentRunner:
         return self.data_loader.load_camera_matrices()
 
     def get_projmats_and_edgemap_info(self, mesh_name, device=torch.device("cpu")):
-        view_conf = self.views_config[mesh_name]
-        edgemap_opts = self.data_loader.edgemap_options[mesh_name]
-        renders = self.data_loader.load_renders(mesh_name)
+        camera_matrices = self.get_camera_matrices()
+        edgemaps, edgemaps_len = self.get_edgemaps(mesh_name)
 
-        view_names = (
-            view_conf["view_names"]
-            if view_conf["mode"] == "manual"
-            else random.sample(view_conf["view_names"], view_conf["num_views"])
-        )
-        print(f"{view_names} from {view_conf['view_names']}")
+        if self.views_config[mesh_name]["mode"] == "manual":
+            view_idx = self.views_config[mesh_name]["view_idx"]
+        else:  # random
+            num_views = self.views_config[mesh_name]["num_views"]
+            view_idx = random.sample(self.views_config[mesh_name]["view_idx"], num_views)
+        print(f"{view_idx} from {self.views_config[mesh_name]['view_idx']}")
 
-        edgemaps, edgemaps_len = load_edgemaps(renders, edgemap_opts)
-        camera_matrices, cam_name_to_id, _ = self.data_loader.load_camera_matrices()
-
-        view_idx = [cam_name_to_id[name] for name in view_names]
-        projmats = torch.stack([camera_matrices[cam_name_to_id[name]]["P"] for name in view_names])
-        tgt_edgemaps = torch.nn.utils.rnn.pad_sequence(
-            [edgemaps[name] for name in view_names],
-            batch_first=True, padding_value=0.0
-        )
-        tgt_edgemaps_len = torch.tensor([edgemaps_len[name] for name in view_names])
+        projmats = torch.stack([camera_matrices[idx]["P"] for idx in view_idx]).to(device)
+        tgt_edgemaps = torch.nn.utils.rnn.pad_sequence([edgemaps[i] for i in view_idx], batch_first=True, padding_value=0.0).to(device)
+        tgt_edgemaps_len = torch.tensor([edgemaps_len[i] for i in view_idx], device=device)
 
         return projmats, (tgt_edgemaps, tgt_edgemaps_len), view_idx
-
 
 
     def get_gt_projmats_and_edgemap_info(self, mesh_name, device=torch.device("cpu")):
-        edgemap_opts = self.data_loader.edgemap_options[mesh_name]
-        renders = self.data_loader.load_renders(mesh_name)
-        edgemaps, edgemaps_len = load_edgemaps(renders, edgemap_opts)
+        camera_matrices = self.get_camera_matrices()
+        edgemaps, edgemaps_len = self.get_edgemaps(mesh_name)
+        view_idx = list(range(len(camera_matrices)))
+        projmats = torch.stack([camera_matrices[idx]["P"] for idx in view_idx]).to(device)
+        tgt_edgemaps = torch.nn.utils.rnn.pad_sequence([edgemaps[i] for i in view_idx], batch_first=True, padding_value=0.0).to(device)
+        tgt_edgemaps_len = torch.tensor([edgemaps_len[i] for i in view_idx], device=device)
 
-        camera_matrices, cam_name_to_id, _ = self.data_loader.load_camera_matrices()
-        view_names = sorted(cam_name_to_id.keys())  # alphabetical order
-        view_idx = [cam_name_to_id[name] for name in view_names]
-
-        projmats = torch.stack([camera_matrices[cam_name_to_id[name]]["P"] for name in view_names]).to(device)
-        tgt_edgemaps = torch.nn.utils.rnn.pad_sequence(
-            [edgemaps[name] for name in view_names],
-            batch_first=True, padding_value=0.0
-        ).to(device)
-        tgt_edgemaps_len = torch.tensor([edgemaps_len[name] for name in view_names], device=device)
-
-        return projmats, (tgt_edgemaps, tgt_edgemaps_len), view_idx
-    
+        return projmats, (tgt_edgemaps, tgt_edgemaps_len), view_idx    
 def smooth_displacement_jacobi(displacement, edge_src, edge_dst, k=1):
     V = displacement.size(0)
     d = displacement.clone()
--- src/framework_mesh/script.py
+++ src/framework_mesh/script.py
@@ -11,7 +11,7 @@ def main():
     device = torch.device("cpu")
 
     data_path = "./framework_mesh/data_noground.json"
-    exp_path = "./framework_mesh/exp_oblique.json"
+    exp_path = "./framework_mesh/exp_penalty.json"
 
     with open(data_path, 'r') as f:
         data_config = json.load(f)
@@ -19,51 +19,51 @@ def main():
     with open(exp_path, 'r') as f:
         exp_config_base = json.load(f)
 
-    mesh_res = 2
     velocity_k = 1
     velocity_beta = 1.0
 
-    data_config['paths']['mesh_res'] = mesh_res
-    dataloader = DataLoader(data_config, device)
+    for mesh_res in [2, 3]:
+        data_config['paths']['mesh_res'] = mesh_res
+        dataloader = DataLoader(data_config, device)
 
-    for constrained in [False, True]:
-        exp_config = json.loads(json.dumps(exp_config_base))  # deep copy
+        # For res=2 run both constraints, for res=3 only unconstrained
+        constrained_options = [False, True] if mesh_res == 2 else [False]
 
-        # Velocity settings (unchanged)
-        exp_config["velocity"] = {
-            "enabled": True,
-            "k": velocity_k,
-            "beta": velocity_beta
-        }
+        for constrained in constrained_options:
+            exp_config = json.loads(json.dumps(exp_config_base))  # deep copy
 
-        # Gradient smoothing
-        exp_config["gradient"] = {
-            "smoothing": True,
-            "method": "jacobi",
-            "k": 5,
-            "constrained": constrained,
-            "debug": False
-        }
+            exp_config["method"] = "penalty"  # ← set penalty method
 
-        # Training hyperparameters
-        exp_config["training"] = {
-            "n_iters": 150,
-            "lr": 5e-5,
-            "momentum": 0.8
-        }
+            exp_config["velocity"] = {
+                "enabled": True,
+                "k": velocity_k,
+                "beta": velocity_beta
+            }
 
-        # Chamfer loss: double-sided
-        exp_config["chamfer"] = {
-            "doublesided": True
-        }
+            exp_config["gradient"] = {
+                "smoothing": True,
+                "method": "jacobi",
+                "k": 5,
+                "constrained": constrained,
+                "debug": False
+            }
 
-        # Naming
-        name = f"clean_vbeta_{velocity_beta}_vk_{velocity_k}_gradsmooth_True_constrained_{constrained}_res_{mesh_res}"
-        exp_config["name"] = name
+            exp_config["training"] = {
+                "n_iters": 150,
+                "lr": 5e-5,
+                "momentum": 0.8
+            }
 
-        print(f"[RUN] {name}")
-        runner = ExperimentRunner(exp_config, dataloader)
-        runner.run()
+            exp_config["chamfer"] = {
+                "doublesided": True
+            }
+
+            name = f"clean_penalty_vbeta_{velocity_beta}_vk_{velocity_k}_gradsmooth_True_constrained_{constrained}_res_{mesh_res}"
+            exp_config["name"] = name
+
+            print(f"[RUN] {name}")
+            runner = ExperimentRunner(exp_config, dataloader)
+            runner.run()
 
 
 if __name__ == "__main__":
